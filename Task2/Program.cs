9a39619b950cf731286622fa84f06b586a7e0b75
Alex_OL <kn211_oos@student.ztu.edu.ua>
2022-11-08 15:05:29 +0200

    Додано Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+Console.WriteLine("Олексюк Олексій");
